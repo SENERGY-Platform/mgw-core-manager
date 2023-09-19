23f45b44157cc16359af14412bf8698269e9a2d0
y-du <42994541+y-du@users.noreply.github.com>
2023-09-19 15:46:39 +0200

    init

---
+++ main.go
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2023 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	gin_mw "github.com/SENERGY-Platform/gin-middleware"
+	"github.com/SENERGY-Platform/go-service-base/srv-base"
+	"github.com/SENERGY-Platform/mgw-core-manager/api"
+	"github.com/SENERGY-Platform/mgw-core-manager/handler/http_hdl"
+	"github.com/SENERGY-Platform/mgw-core-manager/lib/model"
+	"github.com/SENERGY-Platform/mgw-core-manager/util"
+	"github.com/gin-contrib/requestid"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"syscall"
+	"time"
+)
+
+var version string
+
+func main() {
+	ec := 0
+	defer func() {
+		os.Exit(ec)
+	}()
+
+	util.ParseFlags()
+
+	config, err := util.NewConfig(util.Flags.ConfPath)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		ec = 1
+		return
+	}
+
+	logFile, err := util.InitLogger(config.Logger)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		var logFileError *srv_base.LogFileError
+		if errors.As(err, &logFileError) {
+			ec = 1
+			return
+		}
+	}
+	if logFile != nil {
+		defer logFile.Close()
+	}
+
+	util.Logger.Printf("%s %s", model.ServiceName, version)
+
+	util.Logger.Debugf("config: %s", srv_base.ToJsonStr(config))
+
+	watchdog := srv_base.NewWatchdog(util.Logger, syscall.SIGINT, syscall.SIGTERM)
+
+	mApi := api.New()
+
+	gin.SetMode(gin.ReleaseMode)
+	httpHandler := gin.New()
+	staticHeader := map[string]string{
+		model.HeaderApiVer:  version,
+		model.HeaderSrvName: model.ServiceName,
+	}
+	httpHandler.Use(gin_mw.StaticHeaderHandler(staticHeader), requestid.New(requestid.WithCustomHeaderStrKey(model.HeaderRequestID)), gin_mw.LoggerHandler(util.Logger, nil, func(gc *gin.Context) string {
+		return requestid.Get(gc)
+	}), gin_mw.ErrorHandler(http_hdl.GetStatusCode, ", "), gin.Recovery())
+	httpHandler.UseRawPath = true
+
+	http_hdl.SetRoutes(httpHandler, mApi)
+	util.Logger.Debugf("routes: %s", srv_base.ToJsonStr(http_hdl.GetRoutes(httpHandler)))
+
+	listener, err := srv_base.NewUnixListener(config.Socket.Path, os.Getuid(), config.Socket.GroupID, config.Socket.FileMode)
+	if err != nil {
+		util.Logger.Error(err)
+		ec = 1
+		return
+	}
+	server := &http.Server{Handler: httpHandler}
+	srvCtx, srvCF := context.WithCancel(context.Background())
+	watchdog.RegisterStopFunc(func() error {
+		if srvCtx.Err() == nil {
+			ctxWt, cf := context.WithTimeout(context.Background(), time.Second*5)
+			defer cf()
+			if err := server.Shutdown(ctxWt); err != nil {
+				return err
+			}
+			util.Logger.Info("http server shutdown complete")
+		}
+		return nil
+	})
+	watchdog.RegisterHealthFunc(func() bool {
+		if srvCtx.Err() == nil {
+			return true
+		}
+		util.Logger.Error("http server closed unexpectedly")
+		return false
+	})
+
+	watchdog.Start()
+
+	go func() {
+		defer srvCF()
+		util.Logger.Info("starting http server ...")
+		if err := server.Serve(listener); !errors.Is(err, http.ErrServerClosed) {
+			util.Logger.Error(err)
+			ec = 1
+			return
+		}
+	}()
+
+	ec = watchdog.Join()
+}
