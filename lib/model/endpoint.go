d0eb4f751c237056ad6948c9cab5cc5b2cbe8ec4
y-du <42994541+y-du@users.noreply.github.com>
2023-09-28 16:10:34 +0200

    add id and rename deployment id to ref

--- lib/model/endpoint.go
+++ lib/model/endpoint.go
@@ -3,10 +3,11 @@ package model
 type EndpointType = int
 
 type Endpoint struct {
-	Type         EndpointType `json:"type"`
-	DeploymentID string       `json:"d_id"`
-	Host         string       `json:"host"`
-	Port         *int         `json:"port"`
-	IntPath      string       `json:"int_path"`
-	ExtPath      string       `json:"ext_path"`
+	ID      string       `json:"id"`
+	Type    EndpointType `json:"type"`
+	Ref     string       `json:"ref"`
+	Host    string       `json:"host"`
+	Port    *int         `json:"port"`
+	IntPath string       `json:"int_path"`
+	ExtPath string       `json:"ext_path"`
 }
