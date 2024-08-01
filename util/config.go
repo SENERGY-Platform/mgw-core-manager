83147812d7cb058e91800f5e47b548aa37aecaa1
y-du <42994541+y-du@users.noreply.github.com>
2024-08-01 10:01:31 +0200

    add missing log level parser

--- util/config.go
+++ util/config.go
@@ -18,9 +18,12 @@ package util
 
 import (
 	"github.com/SENERGY-Platform/go-service-base/config-hdl"
+	sb_logger "github.com/SENERGY-Platform/go-service-base/logger"
+	envldr "github.com/y-du/go-env-loader"
 	"github.com/y-du/go-log-level/level"
 	"io/fs"
 	"os"
+	"reflect"
 )
 
 type JobsConfig struct {
@@ -95,6 +98,6 @@ func NewConfig(path string) (*Config, error) {
 			Timeout:       10000000000,
 		},
 	}
-	err := config_hdl.Load(&cfg, nil, nil, nil, path)
+	err := config_hdl.Load(&cfg, nil, map[reflect.Type]envldr.Parser{reflect.TypeOf(level.Off): sb_logger.LevelParser}, nil, path)
 	return &cfg, err
 }
