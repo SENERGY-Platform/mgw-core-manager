323e9b248bc2b962e62067233c111aa3fb4d9338
y-du <42994541+y-du@users.noreply.github.com>
2024-10-11 10:02:03 +0200

    add log handlers

---
+++ handler/http_hdl/log.go
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2024 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package http_hdl
+
+import (
+	"github.com/SENERGY-Platform/mgw-container-engine-wrapper/lib/model"
+	"github.com/SENERGY-Platform/mgw-core-manager/lib"
+	"github.com/gin-gonic/gin"
+	"io"
+	"net/http"
+)
+
+const logIdParam = "l"
+
+type logQuery struct {
+	MaxLines int `form:"max_lines"`
+}
+
+func getLogsH(a lib.Api) gin.HandlerFunc {
+	return func(gc *gin.Context) {
+		logs, err := a.ListLogs(gc.Request.Context())
+		if err != nil {
+			_ = gc.Error(err)
+			return
+		}
+		gc.JSON(http.StatusOK, logs)
+	}
+}
+
+func getLogH(a lib.Api) gin.HandlerFunc {
+	return func(gc *gin.Context) {
+		query := logQuery{}
+		if err := gc.ShouldBindQuery(&query); err != nil {
+			_ = gc.Error(model.NewInvalidInputError(err))
+			return
+		}
+		rc, err := a.GetLog(gc.Request.Context(), gc.Param(logIdParam), query.MaxLines)
+		if err != nil {
+			_ = gc.Error(err)
+			return
+		}
+		defer rc.Close()
+		gc.Status(http.StatusOK)
+		gc.Header("Transfer-Encoding", "chunked")
+		gc.Header("Content-Type", gin.MIMEPlain)
+		for {
+			var b = make([]byte, 204800)
+			n, rErr := rc.Read(b)
+			if rErr != nil {
+				if rErr == io.EOF {
+					if n > 0 {
+						_, wErr := gc.Writer.Write(b[:n])
+						if wErr != nil {
+							_ = gc.Error(model.NewInternalError(wErr))
+							return
+						}
+						gc.Writer.Flush()
+					}
+					break
+				}
+				_ = gc.Error(model.NewInternalError(rErr))
+				return
+			}
+			_, wErr := gc.Writer.Write(b)
+			if wErr != nil {
+				_ = gc.Error(model.NewInternalError(wErr))
+				return
+			}
+			gc.Writer.Flush()
+		}
+	}
+}
